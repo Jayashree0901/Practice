5332d2022e0a7b9785f0bb45013f91f6c51b3273
Jayashree <jayashreearunan01@gmail.com>
2024-07-03 22:13:03 +0530

    map new

--- map_reduce_filter.py
+++ map_reduce_filter.py
@@ -1,4 +1,10 @@
 # map function
-t = [3,6,9,2,1,7]
-t1 = list(map(lambda x: x**2, t))
-print(t1)
+# t = [3,6,9,2,1,7]
+# t1 = list(map(lambda x: x**2, t))
+# print(t1)
+
+n1 = [1, 2, 3]
+n2 = [4, 5, 6]
+n3 = [7, 8, 9]
+print(list(map(lambda a,b,c: a+b+c, n1,n2,n3)))
+
